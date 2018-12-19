7c6207d45ecf3a17ce7c04dc7b505479f614fe43
micool91@gmail.com <micool91@gmail.com>
2018-12-19 22:52:15 +0100

    gallery added

---
+++ pages/concerts/index.vue
@@ -0,0 +1,55 @@
+<template>
+<div class="card card-1">
+  <h1>Koncerty</h1>
+  <br>
+  <br>
+  <center><img src="~/assets/concert.jpg" alt="Koncert"></center>
+<br><br><br><br><br>
+</div>
+</template>
+
+
+<style scoped>
+h1 {
+  text-align: center;
+}
+
+.card {
+  background: #fff;
+  border-radius: 2px;
+  display: inline-block;
+  margin: 1rem;
+  position: relative;
+  width: 98%;
+  padding: 30px;
+}
+
+.card-1 {
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+}
+
+a {
+  text-decoration: none;
+  color: #3b8070;
+}
+a:hover {
+  color: #41b883;
+  text-decoration: underline;
+}
+
+p {
+  padding: 10px;
+  font-size: 1.2em;
+}
+
+img {
+    max-height: 800px;
+    padding: 0px;
+    border-top-left-radius: 20px;
+    border-top-right-radius: 20px;
+    border-bottom-left-radius: 20px;
+    border-bottom-right-radius: 20px;
+    box-shadow: 0 1px 30px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+}
+</style>
---
+++ pages/gallery/index.vue
@@ -0,0 +1,88 @@
+<template>
+
+  <div>
+      <br><br><br>
+      <center><h1>Galeria</h1></center>
+      <br>
+      <hr>
+      <br><br>
+      <no-ssr>
+    <gallery :images="images" :index="index" @close="index = null"></gallery>
+    </no-ssr>
+    <div
+      class="image"
+      v-for="(image, imageIndex) in images"
+      :key="imageIndex"
+      @click="index = imageIndex"
+      :style="{ backgroundImage: 'url(' + image + ')', width: '300px', height: '200px' }"
+    ></div>
+
+
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <br><br><br><br><br><br><br><br><br><br>
+  </div>
+
+</template>
+
+<script>
+let VueGallery = {}
+if (process.browser) {
+  VueGallery = require('vue-gallery')
+}
+  
+  export default {
+    data: function () {
+      return {
+        images: [
+          '12.jpg',
+          '13.jpg',
+          '14.jpg',
+          '15.jpg',
+          '16.jpg',
+          '18.jpg',
+          '19.jpg',
+          '20.jpg',
+          '21.jpg',
+          '22.jpg',
+          '23.jpg',
+          '24.jpg',
+          '25.jpg',
+          '30.jpg',
+          '31.jpg',
+          '32.jpg',
+          '33.jpg',
+          '34.jpg',
+          '35.jpg',
+          '36.jpg',
+          '37.jpg',
+          '38.jpg',
+          '39.jpg',
+          '40.jpg',
+          '41.jpg',
+          '42.jpg',
+          '43.jpg',
+          '44.jpg',
+          '45.jpg',
+          '46.jpg'
+        ],
+        index: null
+      };
+    },
+
+    components: {
+      'gallery': VueGallery
+    },
+  }
+</script> 
+
+<style scoped>
+  .image {
+    float: left;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center center;
+    border: 1px solid #ebebeb;
+    margin: 5px;
+  }
+</style>
